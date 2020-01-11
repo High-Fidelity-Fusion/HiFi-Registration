dfd25e5b60c4f12ddda940d6ab58305eda6e045d
blueridger <nath.sears@gmail.com>
2020-01-11 14:43:02 -0800

    Hello World

---
+++ hifireg/hifireg/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('registration.urls')),
+    path('registration/', include('registration.urls')),
+    path('admin/', admin.site.urls),
+]
