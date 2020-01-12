0647f5a35fec23ef95750dd7aa1b725076220f35
blueridger <nath.sears@gmail.com>
2020-01-11 16:12:30 -0800

    First template

--- hifireg/registration/views.py
+++ hifireg/registration/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the registration index.")
+    return render(request, 'registration/index.html', {})
+
