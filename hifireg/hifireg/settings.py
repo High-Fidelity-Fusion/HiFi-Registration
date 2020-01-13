76d6a2c556da2394a2d3a66ddb703e6ec31d5b9b
Bryant Ferguson <bryant.s.ferguson@gmail.com>
2020-01-13 00:40:18 -0800

    Update to mysql database
    Added mysqlclient to Pipfile.
    Updated project settings.py to use mysql engine and to read all of the
    parameters from environment variables.
    In pipenv, the easy way to automatically set up your environment is to
    create a “.env” file in the project root directory that automatically will
    set environment variables for this project when pipenv runs.

--- hifireg/hifireg/settings.py
+++ hifireg/hifireg/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'hifireg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': os.getenv('DBHOST'),
+        'PORT': os.getenv('DBPORT'),
+        'USER': os.getenv('DBUSER'),
+        'PASSWORD': os.getenv('DBPASSWORD'),
+        'NAME': os.getenv('DBNAME'),
     }
 }
 
