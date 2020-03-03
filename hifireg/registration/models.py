543a1eff4c31ba929da71a0f444b6b0b3efe2b25
Thena Seer <nath.sears@gmail.com>
2020-03-03 10:51:55 -0800

    Registration model starter to unblock end-to-end form task

--- hifireg/registration/models.py
+++ hifireg/registration/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Registration(models.Model):
+    agree_to_coc = models.BooleanField(verbose_name="Do you agree to the Code of Conduct?", null=True, blank=False)
+    allergens_severe = models.TextField(verbose_name="Severe Allergies", help_text="List allergens that would be a threat to you if they were in the venue at all", max_length=828, null=True, blank=False)
