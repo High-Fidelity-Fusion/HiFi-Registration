f95ae1b19d264a79fcf664b4eaf7d5b3e6ff3b1c
Zach May <zacdan@comcast.net>
2020-03-12 17:22:25 -0700

    Oops
    Missed these files during commit

--- hifireg/registration/urls.py
+++ hifireg/registration/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
+    path('form',views.form, name='form'),
+    path('submit',views.submit,name='submit')
 ]
--- hifireg/registration/views.py
+++ hifireg/registration/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
+from .forms import RegistrationForm
 
 
 def index(request):
-    return render(request, 'registration/index.html', {})
+    return render(request, 'registration/index.html')
 
 def login(request):
     if request.method == 'POST':
@@ -21,3 +22,15 @@ def login(request):
 
     return render(request, 'registration/login.html', {'form': f})
 
+def form(request):
+    f = RegistrationForm()
+    context = {'registration_form' : f}
+    return render(request,'registration/form.html', context)
+
+def submit(request):
+    f = RegistrationForm(request.POST or None)
+    if f.is_valid():
+        f.save()
+        return render(request,'registration/index.html')
+    else:
+        return render(request, 'registration/form.html'),{'error_message': "Sucks to suck."}
