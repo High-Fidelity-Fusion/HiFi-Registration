1963c23d79d30d571298a626baf9be9c67e3926d
Bryant Ferguson <bryant.s.ferguson@gmail.com>
2020-03-25 00:16:59 -0700

    Add profile parameters to user model

--- hifireg/registration/models/user.py
+++ hifireg/registration/models/user.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Custom User Model
 # https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#substituting-a-custom-user-model
 class User(AbstractUser):
-    pass
+    personal_pronouns = models.CharField(help_text="Your preferred personal pronouns.", max_length=20, null=True, blank=True)
+    city = models.CharField(help_text="Your home city.", max_length=50, null=True, blank=True)
