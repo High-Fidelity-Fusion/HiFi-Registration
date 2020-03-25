4c4f5f2771b2241c8de5096e0f1523869d75c473
Bryant Ferguson <bryant.s.ferguson@gmail.com>
2020-03-25 00:17:34 -0700

    Remove username field
    - Username has been removed and custom objects defined for handling this scenario

--- hifireg/registration/admin.py
+++ hifireg/registration/admin.py
@@ -1,10 +1,36 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as UserAdmin_
+from django.utils.translation import gettext_lazy as _
 
 from .models import Registration
 from .models import User
 
+class UserAdmin(UserAdmin_):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'personal_pronouns', 'city',)}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    readonly_fields = ('email', 'last_login', 'date_joined')
+
+# Register custom User and UserAdmin
+admin.site.register(User, UserAdmin)
 
 # Register your models here.
 admin.site.register(Registration)
-admin.site.register(User, UserAdmin)
+
+
--- hifireg/registration/views.py
+++ hifireg/registration/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
+from .forms import UserCreationForm
 from .forms import RegistrationForm
 
 
 def index(request):
+    # if not logged in -- redirect to make-account
+    if not request.user.is_authenticated:
+        return redirect('create-user')
+
     return render(request, 'registration/index.html')
 
-# def login(request):
-#     if request.method == 'POST':
-#         f = UserCreationForm(request.POST)
-#         if f.is_valid():
-#             f.save()
-#             messages.success(request, 'Account created successfully')
-#             return redirect('login')
+def create_user(request):
+    if request.method == 'POST':
+        f = UserCreationForm(request.POST)
+        if f.is_valid():
+            f.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('login')
 
-#     else:
-#         f = UserCreationForm()
+    else:
+        f = UserCreationForm()
 
-#     return render(request, 'registration/login.html', {'form': f})
+    return render(request, 'registration/create_user.html', {'form': f})
 
 def form(request):
     f = RegistrationForm()
