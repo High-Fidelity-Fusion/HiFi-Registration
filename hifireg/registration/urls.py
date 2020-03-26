4f6ead96d2c78789505084f73788f4dc33294c4d
Bryant Ferguson <bryant.s.ferguson@gmail.com>
2020-03-25 19:22:08 -0700

    Add built user management views and some templates

--- hifireg/registration/urls.py
+++ hifireg/registration/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
-    path('form',views.form, name='form'),
-    path('submit',views.submit,name='submit')
+    # path('login', views.login, name='login'),
+    path('form/', views.form, name='form'),
+    path('submit/', views.submit, name='submit'),
+    path('account/', include('django.contrib.auth.urls')),
 ]
--- hifireg/registration/views.py
+++ hifireg/registration/views.py
@@ -9,18 +9,18 @@ from .forms import RegistrationForm
 def index(request):
     return render(request, 'registration/index.html')
 
-def login(request):
-    if request.method == 'POST':
-        f = UserCreationForm(request.POST)
-        if f.is_valid():
-            f.save()
-            messages.success(request, 'Account created successfully')
-            return redirect('login')
+# def login(request):
+#     if request.method == 'POST':
+#         f = UserCreationForm(request.POST)
+#         if f.is_valid():
+#             f.save()
+#             messages.success(request, 'Account created successfully')
+#             return redirect('login')
 
-    else:
-        f = UserCreationForm()
+#     else:
+#         f = UserCreationForm()
 
-    return render(request, 'registration/login.html', {'form': f})
+#     return render(request, 'registration/login.html', {'form': f})
 
 def form(request):
     f = RegistrationForm()
