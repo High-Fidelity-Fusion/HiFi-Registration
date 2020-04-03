cd70d9f1a3676248d06958b27ec8784e288cabe7
Bryant Ferguson <bryant.s.ferguson@gmail.com>
2020-04-02 21:14:10 -0700

    Add Widget Tweaks library to facilitate styling
    - Widget Tweaks provides concise functions for modifying the generated html of form fields within the templates.
    - This decouples Forms/ModelForms from the styling framework being used.
    - Styling enforcement resides entirely in the templates.

--- hifireg/hifireg/settings.py
+++ hifireg/hifireg/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration.apps.RegistrationConfig'
+    'registration.apps.RegistrationConfig',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
