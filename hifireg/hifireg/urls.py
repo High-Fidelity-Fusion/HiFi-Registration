78ad69e852bfb71f9ff5821789397dacd5a7ff8e
Bryant Ferguson <bryant.s.ferguson@gmail.com>
2020-04-19 19:46:21 -0700

    Redirect root to "index" of our app
    Eventually, we may just put the app at the index, but for now, this is useful.

--- hifireg/hifireg/urls.py
+++ hifireg/hifireg/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import reverse_lazy, reverse
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('registration/', include('registration.urls')),
+    path('', RedirectView.as_view(url=reverse_lazy('index'))), # set default URL
+    path('registration/', include('registration.urls'), name='reg'),
     path('admin/', admin.site.urls),
 ]
 
