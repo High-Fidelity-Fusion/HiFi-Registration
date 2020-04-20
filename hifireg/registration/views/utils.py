fa0efeae22f5591360041c3124ec23e734feb9c8
Bryant Ferguson <bryant.s.ferguson@gmail.com>
2020-04-20 00:22:53 -0700

    Create a generic_form that renders link or post buttons and simplify some user management
    Consolidated the functionality of link_form and post_form and made a more flexible generic_form
    - To make this work, I created some template filters that check the object type of each button as we iterate through them.
    - This makes it easier and more obvious to add buttons that are just links vs post requests.
    - Which, also means we won't need the "novalidate" tag on the html forms
    - This meant I could remove the "cancel" post handling from the update_user view.
    Also, I replaced "User.objects.get(email=request.user) with "request.user" -- correct me if I'm wrong, but I don't see the need for the extra layer of redirection.

---
+++ hifireg/registration/views/utils.py
@@ -0,0 +1,10 @@
+
+class LinkButton:
+    def __init__(self, text, link):
+        self.text = text
+        self.link = link
+
+class SubmitButton:
+    def __init__(self, text, name):
+        self.text = text
+        self.name = name
