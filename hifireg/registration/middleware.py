e20f4f696ce359f4f64f5159f3aadc1a6c1294ed
Thena Seer <nath.sears@gmail.com>
2020-05-04 18:47:25 -0700

    Session expiration and timer

---
+++ hifireg/registration/middleware.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+class RefreshSessions:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        request.session['last_touch'] = datetime.now().timestamp()
+
+        response = self.get_response(request)
+        return response
