6f09809a4d3fa9eadac531c72594db2d37514ef4
connorfrendt <cjacksonfx@yahoo.com>
2020-05-22 12:09:47 -0700

    payment model, invoice model, registration queryset

---
+++ hifireg/registration/models/invoice.py
@@ -0,0 +1,7 @@
+from django.db import models
+from datetime import datetime
+ 
+class Invoice(models.Model):
+   amount = models.PositiveIntegerField()
+   due_date = models.DateTimeField(auto_now_add=True)
+   order = models.ForeignKey('Order', on_delete=models.CASCADE)
