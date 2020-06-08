a24fe80ba88d6485ff5ff0e3e72cb87a06f2ab12
connorfrendt <cjacksonfx@yahoo.com>
2020-06-08 16:02:26 -0700

    dates/prices show up dynamically depending on num of payments

---
+++ hifireg/registration/static/scripts/payment_plan.js
@@ -0,0 +1,90 @@
+const setDefaultPaymentPlan = (payPerMonthId, monthsId, payPerMonthDefaultValue, monthsDefaultValue) => {
+    let checkbox = document.getElementById('checkbox');
+    let isChecked = checkbox.checked;
+    
+    const hiddenForm = document.getElementById('hidden-div');
+
+    if(!isChecked) {
+        hiddenForm.classList.add('hidden-div');
+        let payPerMonthValue = document.getElementById(payPerMonthId);
+        let monthValue = document.getElementById(monthsId);
+        payPerMonthValue.value = payPerMonthDefaultValue;
+        monthValue.value = monthsDefaultValue;
+    }
+
+    if(isChecked) {
+        hiddenForm.classList.remove('hidden-div');
+    };
+
+    calculateAmountPerPayment();
+}
+
+const calculateAmountPerPayment = () => {
+    let orderTotal = document.getElementById('order-total').textContent.replace(/\$/g,'');
+    let selectedPaymentsPerMonth = document.getElementById('ppm-dropdown');
+    let selectedMonths = document.getElementById('months-dropdown');
+
+    for(let i = 0; i < selectedPaymentsPerMonth.children.length; i++) {
+        if(selectedPaymentsPerMonth.children[i].selected) {
+            var selectedPPMValue = parseInt(selectedPaymentsPerMonth.children[i].value);
+        }
+    }
+
+    for(let i = 0; i < selectedMonths.children.length; i++) {
+        if(selectedMonths.children[i].selected) {
+            var selectedMonthsValue = parseInt(selectedMonths.children[i].value);
+        }
+    }
+
+    let numOfPayments = selectedPPMValue * selectedMonthsValue;
+    let amountForEveryPayment = orderTotal / numOfPayments;
+    let individualPayment = (Math.floor(100 * amountForEveryPayment)/100).toFixed(2);
+    let floorSumofAllPayments = individualPayment * numOfPayments;
+    let firstPayment = (Number(individualPayment) + Number(orderTotal - floorSumofAllPayments)).toFixed(2);
+
+    let numOfPaymentsMinusOne = numOfPayments - 1;
+
+    function addDays(date, days) {
+        const copy = new Date(Number(date));
+        return new Date(copy.setDate(copy.getDate() + days));
+    }
+
+    const addMonths = (date, months) => {
+        const copy = new Date(Number(date));
+        return new Date(copy.setMonth(copy.getMonth() + months));
+    }
+      
+    const date = new Date();
+    const dateInTwoWeeks = addDays(date, 14);
+
+    document.getElementById('dueToday').textContent = firstPayment;
+
+    let arrOfDates = [];
+
+    if(selectedPPMValue === 2) {
+        arrOfDates.push(dateInTwoWeeks.toLocaleDateString());
+    }
+
+    for(let i = 1; i <= selectedMonthsValue - 1; i++) {
+        const newDate = addMonths(date, i);
+
+        arrOfDates.push(newDate.toLocaleDateString());
+        
+        if(selectedPPMValue === 2) {
+            const secondNewDate = addMonths(dateInTwoWeeks, i);
+            arrOfDates.push(secondNewDate.toLocaleDateString());
+        }
+    }
+
+    document.getElementById('payment-options').innerHTML = '';
+
+    for(let i = 0; i < numOfPaymentsMinusOne; i++) {
+        document.getElementById('payment-options').innerHTML += `
+            <div>
+                Due
+                <span>${arrOfDates[i]}</span>:
+                $<span>${individualPayment}</span>
+            </div>
+        `;
+    }
+}
