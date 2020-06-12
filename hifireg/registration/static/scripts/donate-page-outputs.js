a290e54574fe5d280e650c10f0f2aaa66430ea3f
Bryant Ferguson <bryant.s.ferguson@gmail.com>
2020-06-11 22:45:36 -0700

    Attach listener via javascript instead of HTML to simplify donation page

--- hifireg/registration/static/scripts/donate-page-outputs.js
+++ hifireg/registration/static/scripts/donate-page-outputs.js
@@ -1,5 +1,14 @@
+"use strict";
+
+window.onload = function() {
+    let donation_el = document.getElementById("id_donation");
+    donation_el.addEventListener("input", calculateOutputs);
+    calculateOutputs();
+}
+
 function calculateOutputs() {
-        var donation = document.getElementById("id_donation").value * 100
-        var subtotal = document.getElementById("id_subtotal").innerHTML.substring(1) * 100
-        document.getElementById('id_total').innerHTML = "$" + ((subtotal + donation) * .01).toFixed(2)
-    }
+    let donation = document.getElementById("id_donation").value * 100;
+    let subtotal = document.getElementById("id_subtotal").innerHTML.substring(1) * 100;
+
+    document.getElementById('id_total').innerHTML = "$" + ((subtotal + donation) * .01).toFixed(2)
+}
