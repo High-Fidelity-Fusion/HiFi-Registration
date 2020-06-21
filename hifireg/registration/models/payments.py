6c3effd263be4fb19b235ca7876e4b60f05e170e
connorfrendt <cjacksonfx@yahoo.com>
2020-06-21 15:11:59 -0700

    deleting payments.py file

--- hifireg/registration/models/payments.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
- 
-class Payments(models.Model):
-   amount = models.PositiveIntegerField()
-   date = models.DateTimeField()
-   registration = models.ForeignKey('Registration', on_delete=models.SET_NULL, null=True)
