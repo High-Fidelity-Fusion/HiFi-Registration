36901cc5029945c0a441744af5f2c62f2fabe2e1
Thena Seer <nath.sears@gmail.com>
2020-06-26 15:46:28 -0700

    Reload on order expiration

--- hifireg/registration/static/scripts/countdown.js
+++ hifireg/registration/static/scripts/countdown.js
@@ -16,8 +16,8 @@
       var distance = countDownDate - now;
 
       // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      //var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
       if (seconds < 10) {seconds = "0"+seconds;}
@@ -28,7 +28,7 @@
       // If the count down is finished, write some text
       if (distance < 0) {
         clearInterval(countDownInterval);
-        document.getElementById("session-expiry-timer").innerHTML = "YOUR ORDER HAS EXPIRED";
+        location.reload();
       }
     }, 1000);
   }
