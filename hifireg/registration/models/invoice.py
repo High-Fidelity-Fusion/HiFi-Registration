819454e97acc3d7cc1d14e60bc219c601cd6eccc
Thena Seer <nath.sears@gmail.com>
2020-06-27 14:41:42 -0700

    Update payment_plan page to a functional mock. Add new forceful browsing mixins.

--- hifireg/registration/models/invoice.py
+++ hifireg/registration/models/invoice.py
@@ -6,4 +6,3 @@ class Invoice(models.Model):
    due_date = models.DateTimeField(auto_now_add=True)
    order = models.ForeignKey('Order', on_delete=models.CASCADE)
    pay_at_checkout = models.BooleanField(default=False)
-   
