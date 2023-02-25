99ce173c42e864867122efbc55c6c54cf2591e33
Thena Seer <11550396+blueridger@users.noreply.github.com>
2023-02-24 18:09:48 -0800

    Increase some Event+Product field max_lengths

--- hifireg/registration/models/event.py
+++ hifireg/registration/models/event.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Event(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=10, primary_key=True)
     policies = models.TextField(null=True, blank=True)
     requires_vaccination = models.BooleanField()
